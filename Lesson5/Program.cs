97b0cf3194197b772fe53043b0b22cd4dc6ac55d
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-01-21 22:34:10 +0200

    Домашняя работа 5-1

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,2 +1,20 @@
-﻿// Тема 5
-// Урок 1 и 2. Константы и литералы
+﻿// Урок 5. Константы и литералы
+
+// ДЗ 1
+
+//Console.Write("Введите количество миллиметров: ");
+//double mm = double.Parse(Console.ReadLine());
+//const double SM = 0.1;
+//const double M = 0.001;
+//const double KM = 0.000001;
+//const double ML = 0.000000621371192;
+//const double YAR = 0.001094;
+//Console.WriteLine($"\t = {mm * SM:F2} см");
+//Console.WriteLine($"\t = {mm * M:F6} м");
+//Console.WriteLine($"\t = {mm * KM:F6} км");
+//Console.WriteLine($"\t = {mm * ML:F6} миль");
+//Console.WriteLine($"\t = {mm * YAR:F6} ярдов");
+
+
+// ДЗ 2
+
