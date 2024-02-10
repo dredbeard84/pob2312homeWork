607329cca5828bdbfcae8ad0a95f37db2dc8659d
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-02-10 14:59:08 +0200

    Добавил дз 6

---
+++ Lesson06_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Тема 6: Преобразования типов и способы
+
+/* Задание 1:
+ * Есть исходный тип данных: string, хранящий число 28. 
+ * Конвертируйте его в следующие типы данных, последовательно:
+ * сначала в byte,
+ * затем из пердыдущего в nint,
+ * затем из пердыдущего в int,
+ * затем из пердыдущего в long,
+ * затем из пердыдущего в double.
+ */
+
+string str = "28";
+byte numByte = byte.Parse(str);
+nint numNInt = numByte;
+int numInt = (int)numNInt;
+long numLong = numInt;
+double numDouble = numLong;
+Console.WriteLine(numByte);
+Console.WriteLine(numNInt);
+Console.WriteLine(numInt);
+Console.WriteLine(numLong);
+Console.WriteLine(numDouble);
---
+++ Lesson06_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Тема 6: Преобразования типов и способы
+
+// дз 6-2
+/* Написать программу конвертор, в котором будут использованы встроенные методы:
+ *      GetType();
+ *      ToString();
+ *      Parse;
+ * По 2 примера их работы. */
+
+Console.Write("Введите число меньше 380: ");
+double numDouble = double.Parse(Console.ReadLine());// Parse - 1
+int numInt = (int)numDouble;
+string str = numDouble.ToString();// ToString - 1
+char sym = (char)numDouble;
+Console.WriteLine("Целое число: " + numInt + " " + numInt.GetType());// GetType - 1
+Console.WriteLine("Строка: " + str + " " + str.GetType());// GetType - 2
+Console.WriteLine("Символ: " + sym + " " + sym.GetType());// GetType - 3
+Console.WriteLine(int.Parse(sym.ToString()));// Parse - 2, ToString- 2
