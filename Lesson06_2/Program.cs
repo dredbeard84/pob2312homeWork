438ec1ee22872a00f43ebc59b0e078c3a9c88f87
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-02-10 16:03:58 +0200

    дз 6-2

--- Lesson06_2/Program.cs
+++ Lesson06_2/Program.cs
@@ -7,12 +7,13 @@
  *      Parse;
  * По 2 примера их работы. */
 
-Console.Write("Введите число меньше 380: ");
+Console.Write("Введите число от 1 до 380: ");
 double numDouble = double.Parse(Console.ReadLine());// Parse - 1
 int numInt = (int)numDouble;
+Console.WriteLine("Целое число из дробного: " + numInt + " " + numInt.GetType());// GetType - 1
 string str = numDouble.ToString();// ToString - 1
-char sym = (char)numDouble;
-Console.WriteLine("Целое число: " + numInt + " " + numInt.GetType());// GetType - 1
-Console.WriteLine("Строка: " + str + " " + str.GetType());// GetType - 2
-Console.WriteLine("Символ: " + sym + " " + sym.GetType());// GetType - 3
-Console.WriteLine(int.Parse(sym.ToString()));// Parse - 2, ToString- 2
+Console.WriteLine("Строка из дробного числа: " + str + " " + str.GetType());// GetType - 2
+int newNumInt = int.Parse(str);
+Console.WriteLine("Целое число из строки: " + newNumInt + " " + newNumInt.GetType());// Parse - 2
+string newStr = newNumInt.ToString();
+Console.WriteLine("Строка из целого числа: " + newStr + " " + newStr.GetType());// ToString - 2
