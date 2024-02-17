3574336d152c3f8f68c028fd0b6b4c5cb8256364
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-02-17 13:30:33 +0200

    Тема 8 (3)

---
+++ Thema_08_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Тема 8
+
+/*      Заданаие 1:
+Получить значение целого числа с консоли и операцией получения остатка от деления на 8, вывести результат в консоль.
+ */
+
+//Console.Write("Введите целое число: ");
+//int number = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Остаток от деления числа {number} на 8: {number % 8}");
+
+
+/*      Задание 2:
+Напишите программу, которая находит сумму и произведение трёх целых чисел, построчно введённых с клавиатуры в консоль.
+ */
+
+//Console.Write("Введите первое целое число: ");
+//int numberOne = int.Parse(Console.ReadLine());
+//Console.Write("Введите второе целое число: ");
+//int numberTwo = int.Parse(Console.ReadLine());
+//Console.Write("Введите третье целое число: ");
+//int numberThree = int.Parse(Console.ReadLine());
+//Console.WriteLine($"Сумма трёх целых чисел равна: {numberOne + numberTwo + numberThree}");
+//Console.WriteLine($"Произведение трёх целых чисел равно: {numberOne * numberTwo * numberThree}");
