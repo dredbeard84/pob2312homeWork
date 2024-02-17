46684df9c86398bd4825e3e840f300a67659c35b
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-02-17 13:07:33 +0200

    Тема 8 (1)

---
+++ Thema_08_1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Тема 8
+
+/*      Заданаие 1: 
+В школьной библиотеке дети получают книги.
+Петя взял 10 книг, Алёша – на 4 книги меньше, чем Петя, а Ваня на 2 книги больше чем Алёша.
+Сколько книг взял каждый мальчик?
+Сколько книг они взяли вместе?
+*/
+
+//int petya = 10;
+//int alesha = petya - 4;
+//int vanya = alesha + 2;
+//int sum = petya + alesha + vanya;
+//Console.WriteLine($"У Пети {petya} книг");
+//Console.WriteLine($"Алёша взял {alesha} книг");
+//Console.WriteLine($"Ваня взял {vanya} книг");
+//Console.WriteLine($"Всего они взяли {sum} книги");
+
+
+/*      Задание 2:
+В первой корзине 23 яблока, а во второй на 11 яблок меньше.
+Сколько яблок в обеих корзинах?
+*/
+
+//int firstBasket = 23;
+//Console.WriteLine($"В первой корзине {firstBasket} яблока");
+//int secondBasket = firstBasket - 11;
+//Console.WriteLine($"Во сторой корзине {secondBasket} яблок");
+//int sum = firstBasket + secondBasket;
+//Console.WriteLine($"В обеих корзинах {sum} яблок");
