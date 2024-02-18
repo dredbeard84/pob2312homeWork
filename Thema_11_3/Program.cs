9ae1c89115366c92b85210e0cc4c441344ad78d3
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-02-18 18:56:47 +0200

    Тема 11 (3)

---
+++ Thema_11_3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Тема 11 (3)
+
+/* Заданаие 1: Сгенерировать случайное целое число в диапазоне от 0 до 5, запросить ещё одно число у пользователя и в случае совпадения вывести на экран информацию о победе, в противном случае - поражении. 
+ */
+
+//Random random = new Random();
+//int x = random.Next(0, 6);
+//Console.Write("Введите целое число от 0 до 5: ");
+//int a = int.Parse(Console.ReadLine());
+//if (a == x) Console.WriteLine($"Победа!: {a} совпадает с {x}");
+//else Console.WriteLine($"Поражение: {a} не совпадает с {x}");
+
+
+/* Заданаие 2: Запросить у пользователя порядковый номер текущего месяца и вывести его название.
+ Если пользователь указал месяц из зимнего периода, а средняя температура > 0, вывести сообщение «Дождливая зима». 
+ */
+
+//Console.Write("Ведете номер текущего месяца: ");
+//int m = int.Parse(Console.ReadLine());
+//if (m == 1) Console.WriteLine("Январь");
+//else if (m == 2) Console.WriteLine("Февраль");
+//else if (m == 3) Console.WriteLine("Март");
+//else if (m == 4) Console.WriteLine("Апрель");
+//else if (m == 5) Console.WriteLine("Май");
+//else if (m == 6) Console.WriteLine("Июнь");
+//else if (m == 7) Console.WriteLine("Июль");
+//else if (m == 8) Console.WriteLine("Август");
+//else if (m == 9) Console.WriteLine("Сентябрь");
+//else if (m == 10) Console.WriteLine("Октябрь");
+//else if (m == 11) Console.WriteLine("Ноябрь");
+//else if (m == 12) Console.WriteLine("Декабрь");
+//else Console.WriteLine("ошибка ввода");
+
+//if (m == 12 || m == 1 || m == 2) Console.WriteLine("Средняя температура > 0?: \n" +
+//    "да / yes\n" +
+//    "нет / no");
+//string answer = Console.ReadLine();
+//if (answer == "да" || answer == "yes") Console.WriteLine("Дождливая зима");
+//else if (answer == "нет" || answer == "no") Console.WriteLine("зима");
+//else Console.WriteLine("ошибка ввода");
