d70bc22bace3cb13830f9336f4b4769775f29bd5
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-02-28 21:15:36 +0200

    Тема 12 (1)

---
+++ Thema_12_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Тема 12 (1)
+
+/*  Калькулятор: 
+    пользователь может выбрать операцию (сложение, вычитание, умножение, деление),
+    ввести два числа и
+    получить результат.*/
+
+Console.Write("Введите 1-е число: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите операцию: ");
+char oper = char.Parse(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+double b = double.Parse(Console.ReadLine());
+switch (oper)
+{
+    case '+': Console.WriteLine($"{a} + {b} = {a + b}"); break;
+    case '-': Console.WriteLine($"{a} - {b} = {a - b}"); break;
+    case '*': Console.WriteLine($"{a} * {b} = {a * b}"); break;
+    case '/': Console.WriteLine($"{a} / {b} = {(a / b):F2}"); break;
+    default: Console.WriteLine("Такой операции нет"); break;
+}
