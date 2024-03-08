88dabb445e5bfe6da4b6c292daf130d5e5c54809
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-03-08 16:58:59 +0200

    Тема 16 (1)

---
+++ Thema_14_2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Тема 14 (2)
+
+/* Задание 1. Напишите программу, случайным образом генерирующую температуры от 1 до 100.
+			  В случае превышения, устройство останавливает работу с выводом значения критической температуры на экран.
+		      Алгоритм работы:
+				● Создать экземпляр класса Random. (Random rnd = new Random();)
+				● Инициализировать переменную temperature значением 0.
+				● Запустить цикл, начиная с i = 1.
+				● В теле цикла:
+					a. Сгенерировать случайное число в диапазоне от 1 до 100 и сохранить его в переменную temperature.
+					b. Вывести на экран текущую температуру.
+					c. Проверить, превышает ли текущая температура критическую (больше 90).
+					d. Если текущая температура превышает критическую, вывести сообщение об остановке устройства и прервать цикл. */
+
+//Random random = new Random();
+//double temperature = 0;
+//for (int i = 1; ; i++)
+//{
+//    temperature = random.NextDouble() * 100;
+//    Console.WriteLine($"Текущая температура: {temperature:F2}");
+//    if (temperature > 90)
+//    {
+//        Console.Write("(критическая температура)");
+//        break;
+//    }
+//}
+//Console.WriteLine();
+
+
+// Задание 2. Освоить перебор вложенными циклами, переписать код из второго файла и описать алгоритм работы (что за чем идёт).
+
+Console.Write("Укажите количество квадратов: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+Console.Write("Укажите сторону квадрата: ");
+int weigth = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < quantity; i++)
+{
+    for (int j = 0; j < weigth; j++)
+    {
+        Console.Write("*");
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+    for (int k = 0; k < weigth - 2; k++)
+    {
+        Console.Write("*");
+        for (int l = 0; l < weigth + weigth - 2; l++)
+        {
+            Console.Write(" ");
+        }
+        Console.Write("*");
+        Console.WriteLine();
+    }
+    for (int m = 0; m < weigth; m++)
+    {
+        Console.Write("*");
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
---
+++ Thema_16_1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Тема 16 (1)
+
+/* # 1.
+    Создать двумерный массив 3х3.
+    Заполнить его дробными числами от 0 до 20.
+    Оформить вывод на экран всех чисел.
+    Добавить возможность замены значения пользователем.
+    Вывод нового массива. */
+
+//Random random = new Random();
+//double[,] mas = new double[3, 3];
+//for (int i = 0; i < mas.GetLength(0); i++)
+//{
+//    for (int j = 0; j < mas.GetLength(1); j++)
+//    {
+//        mas[i, j] = random.NextDouble() * 20;
+//        Console.Write($"{mas[i, j]:F2} ");
+//    }
+//    Console.WriteLine();
+//}
+//Console.WriteLine();
+//Console.Write($"Введите номер строки (<= {mas.GetLength(0)}): ");
+//int n = int.Parse(Console.ReadLine());
+//Console.Write($"Введите номер столбца (<= {mas.GetLength(0)}): ");
+//int m = int.Parse(Console.ReadLine());
+//Console.Write("Введите новое значение: ");
+//int k = int.Parse(Console.ReadLine());
+//Console.WriteLine();
+//mas[n - 1, m - 1] = k;
+//for (int i = 0; i < mas.GetLength(0); i++)
+//{
+//    for (int j = 0; j < mas.GetLength(1); j++)
+//    {
+//        Console.Write($"{mas[i, j]:F2} ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+/* # 2. 
+    Создать массив символов 5х2.
+    Заполнить его символами по желанию в разнобой.
+    Вывести на экран понятное слово. */
+
+//char[,] mas =
+//{
+//    { 'р', '!' },
+//    { 'а', 'р' },
+//    { 'п', 'м' },
+//    { 'г', 'о' },
+//    { 'м', 'а' }
+//};
+//Console.WriteLine($"{mas[2, 0]}{mas[0, 0]}{mas[3, 1]}{mas[3, 0]}{mas[1, 1]}{mas[4, 1]}{mas[2, 1]}{mas[4, 0]}{mas[1, 0]}{mas[0, 1]}");
