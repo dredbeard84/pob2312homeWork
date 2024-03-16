088ef4ac02c4e566ec07f1a263ea67e21dd9ffa9
Иван Непомилуев <nepomiluev.ivan@gmail.com>
2024-03-16 15:54:34 +0200

    Тема 14 (1)

--- Thema_14_1/Program.cs
+++ Thema_14_1/Program.cs
@@ -1,32 +1,16 @@
-﻿//for (int i = 48; i <= 57; i++) Console.Write((char)i + " ");// 0 1 2 3 4 5 6 7 8 9
-//Console.WriteLine();
-//for (int i = 58; i <= 64; i++) Console.Write((char)i + " ");// : ; < = > ? @
-//Console.WriteLine();
-//for (int i = 65; i <= 90; i++) Console.Write((char)i + " ");// A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-//Console.WriteLine();
-//for (int i = 91; i <= 96; i++) Console.Write((char)i + " ");// [ \ ] ^ _ `
-//Console.WriteLine();
-//for (int i = 97; i <= 122; i++) Console.Write((char)i + " ");// a b c d e f g h i j k l m n o p q r s t u v w x y z
-//Console.WriteLine();
+﻿// Тема: 14 (1)
 
+/* Нужно разработать программу, которая будет генерировать пароли заданной длины для пользователей.
+   Требования к программе:
+   Пользователь должен иметь возможность указать длину пароля при запуске программы.
+   Пароль должен состоять из букв латинского алфавита (в верхнем и нижнем регистре) и цифр.
+   Для реализации генерации пароля требуется использовать цикл for.
+   Программа должна создавать случайный набор символов, выбирая из списка допустимых символов на каждой итерации цикла. */
 
-// 14-1
 Console.Write("Введите длину пароля: ");
 int passLength = int.Parse(Console.ReadLine());
+string symbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 Random random = new Random();
-for (int i = 0; i < passLength; i++)
-{
-    int pass = random.Next(48, 123);
-    if (pass >= 58 && pass <= 64 && pass >= 91 && pass <= 96) Console.Write("");
-    Console.Write((char)pass);
-}
-Console.WriteLine();
-
-
-//Console.Write("ВВедите Х: ");
-//double x = double.Parse(Console.ReadLine());
-//Console.Write("Введите Y: ");
-//double y = double.Parse(Console.ReadLine());
-//if (x >= -1 && x <= 1.5 && y >= -0.5 && y <= 1.5)
-//     Console.WriteLine("Попал");
-//else Console.WriteLine("Не попал");
+string pass = "";
+for (int i = 0; i < passLength; i++) pass += symbols[random.Next(symbols.Length)];
+Console.WriteLine(pass);
